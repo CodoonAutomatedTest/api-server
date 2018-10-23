d4def0c56163c1133f5c9d6c18658215d1681ce3
Mr. X <xiaoqiang@ecs.com>
2018-10-23 10:32:40 +0800

    添加MVC业务模型

---
+++ database/migrations/1540261436718_notes_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class NotesSchema extends Schema {
+  up () {
+    this.create('notes', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('notes')
+  }
+}
+
+module.exports = NotesSchema
